04b2bfa6f79a2c596eec7c40b161ab41cfc3fb09
lxhlxh520 <71735332@qq.com>
2025-05-22 16:45:06 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://lxhlxh520.github.io/AutoxDoc',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/AutoxDoc',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -36,15 +36,15 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/aiselp/AutoxDoc/tree/main',
+          //editUrl:
+            //'https://github.com/aiselp/AutoxDoc/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/aiselp/AutoxDoc/tree/main',
+          //editUrl:
+           // 'https://github.com/aiselp/AutoxDoc/tree/main',
         },
         theme: {
           customCss: './src/css/custom.css',
