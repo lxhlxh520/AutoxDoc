e6d5feb13f131e99e5acc25866b5ffefa69870bd
lxhlxh520 <71735332@qq.com>
2025-05-22 16:56:49 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://lxhlxh520.github.io/AutoxDoc',
+  url: 'https://github.com/lxhlxh520',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/AutoxDoc/',
